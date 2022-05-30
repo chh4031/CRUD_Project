ffc659eabe4991bc59bf78fef620e88c2a89c84d
chh4031 <chh4031@gmail.com>
2022-05-30 21:04:51 +0900

    v 1.1 (업데이트)

--- CRUD/admin.js
+++ CRUD/admin.js
@@ -9,5 +9,45 @@ console.log(a1, a2);
 */
 
 //로컬 스토리지를 이용해 키값으로 값을 받으면 된다!!!
+/*var pt = 1;
+
+var admin_value = document.getElementById('admin_value');
 var a1 = localStorage.getItem('ky1');
-console.log(a1);
+var new_div = document.createElement('div');
+
+new_div.innerHTML = pt+'<div>' + a1 + '</div>';
+admin_value.appendChild(new_div);
+
+pt++;
+*/
+/*
+let a1 = localStorage['keys'];
+
+let data = JSON.parse(a1);
+
+data['name']
+data['id']
+data['pw']
+*/
+var admin_value = document.getElementById('admin_value');
+var i = 0, k = 0, g = 0, m;
+let savearr = [];
+k = localStorage.getItem('k');//k값 즉 i의 값 받기
+
+for(g = 0;g < k;g++){
+    var a1 = [];
+    a1[i] = localStorage.getItem(i);
+    //console.log(a1[i]);
+
+    //admin_value.innerHTML += '<div>' + a1[i] + '</div>';
+
+    k = localStorage.getItem('k');
+    savearr[i] = a1[i];
+
+    i++;
+
+    console.log('k값'+k);
+    console.log('i값'+i);
+    console.log('데이터가 들어간 배열값'+a1);
+}
+console.log(savearr);
--- CRUD/main.js
+++ CRUD/main.js
@@ -1,18 +1,41 @@
 var user_name = document.getElementById("user_name");
 var user_id = document.getElementById("user_id");
 
-var submit = document.getElementById("submit");
 var admin = document.getElementById("admin_value");
 
-submit.onclick = bringvalue;
+var i = 0;
+var k = 0;
+
 
 function bringvalue(){
     var user_name_value = user_name.value;
     var user_id_value = user_id.value;
     console.log(user_name_value);
-    console.log(user_id_value);
+    //console.log(user_id_value);
     //location.href="admin.html?" + user_name_value + ":"+ user_id_value 이방식 안씀
-    localStorage.setItem('ky1', user_name_value);//로컬 스토리지 이용!!!
+    
+    /*
+    localStorage['keys'] = JSON.stringify({
+        'name': '이름',
+        'id': '아이디',
+        'pw': '비밀번호'
+    })
+
+    localStorage['name']
+    localStorage['id']
+    localStorage['pw']
+    */
+    if(localStorage.getItem('k')!=0){
+        i = localStorage.getItem('k');
+        localStorage.setItem(i++, user_name_value);
+        localStorage.setItem('k',i);
+    }else{
+        localStorage.setItem(i++, user_name_value);//i번째 이름값을 보내기
+        localStorage.setItem('k',i);//k라는 이름의 i값을 보내기
+    }
+    //로컬 스토리지 이용!!!
+    //location.href='main.html';
+    console.log(i);
 }
 
 function preventClick(e){
